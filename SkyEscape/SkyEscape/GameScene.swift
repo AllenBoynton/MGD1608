0b9b2e128b0b489b04e99a9f21d956ef7cdf7d39
Allen Boynton <aboynton@fullsail.edu>
2016-08-05 23:25:36 -0400

    Removed excess code...neatened up.
    Removed redundancy, useless code, made sure all was commented that
    needed to be.

--- SkyEscape/SkyEscape/GameScene.swift
+++ SkyEscape/SkyEscape/GameScene.swift
@@ -25,17 +25,19 @@ let starMask    : UInt32 = 0x1 << 4 // 16
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    // The scroll version of the nodes will become a scrolling, repeating backround
+    
     // Sky View
     var myBackground = SKSpriteNode()
     var myBackgroundScroll = SKSpriteNode()
     
-    // Most foreground mountains
-    var ground = SKSpriteNode()
-    var groundScroll = SKSpriteNode()
-    
     // Single background mountain
     var mountain = SKSpriteNode()
     var mountainScroll = SKSpriteNode()
+    
+    // Most foreground mountains
+    var ground = SKSpriteNode()
+    var groundScroll = SKSpriteNode()
     
     // All collision nodes
     var star = SKSpriteNode()
@@ -66,34 +68,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
         // Calls the sprite to its child node name
         star = (self.childNodeWithName("star") as! SKSpriteNode)
         badCloud = (self.childNodeWithName("badCloud") as! SKSpriteNode)
-        
+        myBackground = SKSpriteNode(imageNamed: "cartoonCloudsBG") // Main Background
+        mountain = (self.childNodeWithName("mountain") as! SKSpriteNode) // Single mountain
+        ground = (self.childNodeWithName("ground") as! SKSpriteNode) // Foreground mountains
+
         self.physicsWorld.contactDelegate = self // Physics delegate set
-        
-        // Main Background
-        myBackground = SKSpriteNode(imageNamed: "cartoonCloudsBG")
-//        myBackground.anchorPoint = CGPointZero
-//        myBackground.position = CGPointMake(self.size.width/2, self.size.height/2)
-//        self.addChild(myBackground)
-//        
-//        // Single mountain view background - mid-ground
-        mountain = (self.childNodeWithName("mountain") as! SKSpriteNode)
-//        mountain.anchorPoint = CGPointZero
-//        mountain.position = CGPointMake(500, 0)
-//        self.addChild(mountain)
-//        
-//        // Foreground creation
-        ground = (self.childNodeWithName("ground") as! SKSpriteNode)
-//        ground.anchorPoint = CGPointZero
-//        ground.position = CGPointMake(0, 0)
-//        self.addChild(ground)
-//        
+
         // Animation atlas
         PlaneAtlas = SKTextureAtlas(named:"Images")
         
-        for i in 1...PlaneAtlas.textureNames.count {
+        for i in 1...PlaneAtlas.textureNames.count { // Iterates loop for plane animation
             let Plane = "myPlane\(i).png"
             PlaneArray.append(SKTexture(imageNamed: Plane))
         }
@@ -107,12 +95,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Define and repeat animation
         MyPlane.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(PlaneArray, timePerFrame: 0.05)))
-                
+        
         // Create a smoke trail for plane
         let smokeTrail = SKEmitterNode(fileNamed: "SmokeTrail")
         
         //the y-position needs to be slightly behind the plane
-        smokeTrail!.position = CGPointMake(-190.0, -20.0)
+        smokeTrail!.position = CGPointMake(0.0, 0.0)
         smokeTrail!.xScale = 2.0
         smokeTrail!.yScale = 2.0
         
@@ -129,7 +117,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let action2 = SKAction.rotateByAngle(CGFloat(rads2), duration: 0.25) //Rotation opp. way
         let sequence = SKAction.sequence([action1, action2]) // Adds actions together
         star.runAction(SKAction.repeatActionForever(sequence)) // Runs sequence action forever
-        
         
         // Adds background sound to game
         let backgroundMusic = SKAudioNode(fileNamed: "bgMusic.mp3")
@@ -152,51 +139,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         touchLocation = touches.first!.locationInNode(self)
-        
-        // Adds bomb to drop from myPlane
-//                let bomb: SKSpriteNode = SKScene(fileNamed: "Bomb")!.childNodeWithName("bomb")! as! SKSpriteNode
-//                bomb.removeFromParent()
-//                self.addChild(bomb)
-//        
-//                // Sets position of bomb at myPlane
-//                bomb.zPosition = 0
-//                bomb.position = MyPlane.position
-//                let angleInRadians2 = Float(MyPlane.zRotation)
-//                let speed2 = CGFloat(50.0)
-//                let vx2: CGFloat = CGFloat(cosf(angleInRadians2)) * speed2
-//                let vy2: CGFloat = CGFloat(sinf(angleInRadians2)) * speed2
-//                bomb.physicsBody?.applyImpulse(CGVectorMake(vx2, vy2))
-        
-        
-        let nodes: [AnyObject] = self.nodesAtPoint(touchLocation)
-        if nodes == star {
-            // Adding sound when star is tapped or flown into
-            self.runAction(SKAction.playSoundFileNamed("star.mp3", waitForCompletion: true))
-            backgroundMusic.runAction(SKAction.pause())
-        }
-//        else if nodes == badCloud {
-            // Adding sound when star is tapped or flown into
-//            self.runAction(SKAction.playSoundFileNamed("thump.mp3", waitForCompletion: false))
-//            backgroundMusic.runAction(SKAction.pause())
-//            planeNoise.runAction(SKAction.pause())
-//            bulletSound.runAction(SKAction.pause())
-//        }
-//        else if nodes == mountain {
-            // Adding sound when star is tapped or flown into
-//            self.runAction(SKAction.playSoundFileNamed("crash.mp3", waitForCompletion: false))
-//            backgroundMusic.runAction(SKAction.pause())
-//            planeNoise.runAction(SKAction.pause())
-//            bulletSound.runAction(SKAction.pause())
-//        }
-        
-        // Adding sound to the gun fire
-//        self.runAction(SKAction.playSoundFileNamed("gunfire.mp3", waitForCompletion: true))
-        
-        // Adding sound to the bomb fall
-//        self.runAction(SKAction.playSoundFileNamed("bomb.mp3", waitForCompletion: true))
-        
-//        // Adding sound when star is tapped or flown into
-//        self.runAction(SKAction.playSoundFileNamed("star.mp3", waitForCompletion: true))
     }
    
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -227,51 +169,34 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Adding sound to the cannon fire
         self.runAction(SKAction.playSoundFileNamed("gunfire.mp3", waitForCompletion: true))
-        
-        // Starting and stopping background sound
-//        backgroundMusic.runAction(SKAction.stop())
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-//        let percent = touchLocation.x / size.width
-//        let newAngle = percent * 90 - 90
-//        MyPlane.zRotation = CGFloat(newAngle) * CGFloat(M_PI) / 90.0
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
-        
+        // Function for what to do when one node makes contact or collides with another
         let bullet = (contact.bodyA.categoryBitMask == bulletMask) ? contact.bodyA: contact.bodyB
         let other = (bullet == contact.bodyA) ? contact.bodyB: contact.bodyA
-        if other.categoryBitMask == starMask {//|| other.categoryBitMask == orangePegMask {
+        if other.categoryBitMask == starMask {
             self.didHitNode(other)
             // Adds star to make sound
             let star: SKSpriteNode = SKScene(fileNamed: "Star")!.childNodeWithName("star")! as! SKSpriteNode
             star.removeFromParent()
             self.addChild(star)
         }
-//        else if other.categoryBitMask == squareMask {
-//        }
-//        else if other.categoryBitMask == wallMask {
-//        }
-//        else if other.categoryBitMask == ballMask {
-//        }
-//        else if other.categoryBitMask == bucketMask {
-//        }
     }
     
+    // Function for collision
     func didHitNode(star: SKPhysicsBody) {
-//        let blue = UIColor(red: 0.16, green: 0.73, blue: 0.78, alpha: 1.0)
-//        let orange = UIColor(red: 1.0, green: 0.45, blue: 0.0, alpha: 1.0)
-        
+        // The star will emit a spark when hit
         let spark: SKEmitterNode = SKEmitterNode(fileNamed: "Explode")!
         spark.position = star.node!.position
-//        spark.particleColor = (peg.categoryBitMask == orangePegMask) ? orange : blue
         self.addChild(spark)
         star.node?.removeFromParent()
     
         // Adding sound to the star hits
         self.runAction(SKAction.playSoundFileNamed("star.mp3", waitForCompletion: true))
     }
-
 }
